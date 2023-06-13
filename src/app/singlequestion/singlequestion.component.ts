57fbb2d31c3425ff673ddb9ca9202707f8224822
samndambuki <sam.ndambuki08@gmail.com>
2023-06-13 08:04:23 +0300

    single question validations done

--- src/app/singlequestion/singlequestion.component.ts
+++ src/app/singlequestion/singlequestion.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { faArrowUp, faCaretDown, faCaretUp, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-singlequestion',
   standalone: true,
-  imports: [CommonModule,FontAwesomeModule],
+  imports: [CommonModule,FontAwesomeModule,ReactiveFormsModule],
   templateUrl: './singlequestion.component.html',
   styleUrls: ['./singlequestion.component.css']
 })
@@ -17,7 +18,27 @@ export class SinglequestionComponent {
   upIcon = faCaretUp
   downIcon = faCaretDown
 
-  constructor(private router:Router){}
+  answerForm!: FormGroup;
+
+  constructor(private router:Router,private formBuilder: FormBuilder){}
+
+  ngOnInit() {
+    this.answerForm = this.formBuilder.group({
+      answerInputField: ['', [Validators.required, Validators.minLength(10)]]
+    });
+  }
+
+  get answerInputField() {
+    return this.answerForm.get('answerInputField');
+  }
+
+  submitAnswer() {
+    if (this.answerForm.valid) {
+      const answer = this.answerForm.value.answerInputField;
+      console.log('Submitted Answer:', answer);
+      this.answerForm.reset();
+    }
+  }
 
   //method to handle home button click event
   onHomeButtonClicked(){
