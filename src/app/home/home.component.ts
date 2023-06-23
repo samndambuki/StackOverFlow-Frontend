ee56889c0de21fe9fd395c0138cfe728ed04663a
samndambuki <sam.ndambuki08@gmail.com>
2023-06-23 10:33:45 +0300

    ask question css updates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,FontAwesomeModule],
+  imports: [CommonModule,FontAwesomeModule,RouterModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
@@ -17,45 +17,5 @@ export class HomeComponent {
 
   //inject router to handle navigation 
   constructor(private router:Router){}
-
-  //method to handle tags button click event
-  onTagsClicked(){
-    this.router.navigate(['/tags'])
-  }
-
-  //method to handle my questions click event
-  onMyQuestionsClicked(){
-    this.router.navigate(['/myquestions'])
-  }
-
-  //method to handle ask question click event
-  onAskQuestionClicked(){
-    this.router.navigate(['/askquestion'])
-  }
-
-  //method to handle single question clcik event
-  onSingleQuestionClicked(){
-    this.router.navigate(['/singlequestion'])
-  }
-
-  //method to handle logout click event
-  onLogoutClicked(){
-    this.router.navigate(['/landing'])
-  }
-
-  //nvigate to admin page
-  onAdminButtonClicked(){
-    this.router.navigate(['admin'])
-  }
-
-  //method to handle home button click event
-  onHomeButtonClicked(){
-    this.router.navigate(['home'])
-  }
-
-  //method to handle profile click event
-  onProfileButtonClciked(){
-    this.router.navigate(['userprofile'])
-  }
 
 }
--- src/app/my-questions/my-questions.component.ts
+++ src/app/my-questions/my-questions.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-my-questions',
   standalone: true,
-  imports: [CommonModule,FontAwesomeModule],
+  imports: [CommonModule,FontAwesomeModule,RouterModule],
   templateUrl: './my-questions.component.html',
   styleUrls: ['./my-questions.component.css']
 })
@@ -18,39 +18,5 @@ export class MyQuestionsComponent {
   //inject router to handle navigation
   constructor(private router:Router){}
 
-  //method to handle askquestion click event
-  onAskQuestionClicked(){
-    this.router.navigate(['/askquestion'])
-  }
-
-  //method to handle single question click event
-  onSingleQuestionClikced(){
-    this.router.navigate(['/singlequestion'])
-  }
-
-  //method to handle update question click event
-  onUpdateClicked(){
-    this.router.navigate(['/updatequestion'])
-  }
-
-  //method to handle home button click event
-  onHomeButtonClicked(){
-    this.router.navigate(['home'])
-  }
-
-  //method to handle tags button click event
-  onTagsButtonClicked(){
-    this.router.navigate(['tags'])
-  }
-
-  //method to handle profile button click event
-  onMyProfileButtonClciked(){
-    this.router.navigate(['userprofile'])
-  }
-
-  //method to implement logout button click event
-  onLogoutButtonClicked(){
-    this.router.navigate(['landing'])
-  }
 
 }
